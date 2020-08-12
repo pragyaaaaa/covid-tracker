031c103daf06aefce86a139db1179cc3797f37b6
pragyaaaaa <pragyasabstracts@gmail.com>
2020-08-12 13:05:10 +0530

    --structure

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="app">
+      {/* Header: Title+select input dropdown field */}
+      {/* InfoBox x 3 */}
+      {/* Table */}
+      {/* Graph */}
+      {/* Map */}
     </div>
   );
 }

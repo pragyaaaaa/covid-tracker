7e228a8ebd356f45fecfe7e8344fa65e9fc110ff
pragyaaaaa <pragyasabstracts@gmail.com>
2020-08-12 13:44:12 +0530

    --API call for populating country names.

---
+++ projectDescription.md
@@ -0,0 +1,5 @@
+# covid-tracker
+
+1.  How to get dropdown using material UI?
+2.  How to make an API call?
+2.  For displaying items side-by-side use flex.
--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+
+//https://disease.sh/v3​/covid-19​/countries
+//useeffect: rumns a piece of code based on a given condition
+
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountryInfo = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso2
+            }
+          ));
+          setCountries(countries);
+        })
+    }
+    getCountryInfo();
+  }, []);
   return (
     <div className="app">
+      <div className="app-header">
+        <h1>COVID Tracker</h1>
+        {/* dropdown using material UI */}
+        <FormControl className="app-dropdown">
+          <Select
+            variant="outlined"
+            value="covid">
+            {
+              countries.map(country => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">A</MenuItem>
+            <MenuItem value="worldwide">B</MenuItem>
+            <MenuItem value="worldwide">C</MenuItem>
+            <MenuItem value="worldwide">D</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+
+
       {/* Header: Title+select input dropdown field */}
       {/* InfoBox x 3 */}
       {/* Table */}
